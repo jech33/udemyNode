d5d199a5bb93bd7b379408679ce8d91cb73c8f37
Javier Echavez Chardaux <javier.echavez@ludycom.com>
2022-01-04 09:50:29 -0500

    Webserver ready to deploy

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const hbs = require('hbs');
+
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 
 // TODO require('hbs')
 app.set('view engine', 'hbs');
